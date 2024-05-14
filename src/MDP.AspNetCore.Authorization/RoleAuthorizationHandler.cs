43c8f327cd49a864e014fdca6ca62cc483876bfb
clark159 <clark159@hotmail.com>
2024-05-14 11:19:15 +0800

    [src] MDP.AspNetCore.Authorization: 調整程式排版

--- src/MDP.AspNetCore.Authorization/RoleAuthorizationHandler.cs
+++ src/MDP.AspNetCore.Authorization/RoleAuthorizationHandler.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
-using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
