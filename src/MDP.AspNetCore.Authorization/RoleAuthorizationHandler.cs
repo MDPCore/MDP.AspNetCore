74eead31a98623c776f31694a31f42fbe81156d0
clark159 <clark159@hotmail.com>
2024-05-20 12:23:14 +0800

    [src] MDP.AspNetCore.Authorization: 加入DefaultRoleAssignmentProvider.Instance

--- src/MDP.AspNetCore.Authorization/RoleAuthorizationHandler.cs
+++ src/MDP.AspNetCore.Authorization/RoleAuthorizationHandler.cs
@@ -30,7 +30,7 @@ namespace MDP.AspNetCore.Authorization
             _roleAssignmentProviderList = roleAssignmentProviderList;
             if (_roleAssignmentProviderList.Count <= 0)
             {
-                _roleAssignmentProviderList.Add(new DefaultRoleAssignmentProvider());
+                _roleAssignmentProviderList.Add(DefaultRoleAssignmentProvider.Instance);
             }
 
             // AccessPermissionProviderList            
