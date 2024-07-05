947671fbc31e16ad13f769388cc82038062e948f
clark159 <clark159@hotmail.com>
2024-07-05 17:12:40 +0800

    [nuget] Release MDP.AspNetCore 8.0.7.14

--- build/NuGet-Delivery.bat
+++ build/NuGet-Delivery.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 
 :: ================================================
 :: Variables
-set version=8.0.7.13
+set version=8.0.7.14
 
 set buildDir=%~dp0
 set srcDir=%~dp0..\src
